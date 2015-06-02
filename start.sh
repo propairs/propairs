dc2952e2376894080271d2f437dca37a35f3df34
fkrull <fkrull>
2015-06-02 21:58:38 +0200

    model-merge use new log

--- start.sh
+++ start.sh
@@ -184,7 +184,7 @@ if [ ${rebuild_pdb} -eq 1 ]; then
    mkdir -p pdb_bio_merged
    mkdir -p pdb_dst
    python $PROPAIRSROOT/pdb-merge-bio/merge_bio_folder.py --numthreads ${NUMCPU}
-   ${PROPAIRSROOT}/bin/pdbbio_merge_model.sh pdb pdb_bio_merged/ pdb_dst
+   ${PROPAIRSROOT}/bin/pdbbio_merge_model.sh pdb pdb_bio_merged/ pdb_dst | pplog 1
    # store md5sums for next call
    cp ./pdb.md5     ./pdb.md5_old
    cp ./pdb_bio.md5 ./pdb_bio.md5_old
