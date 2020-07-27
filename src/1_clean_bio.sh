dbbec5c65da7b4e9107a16f4c9c84d955075eb90
Florian Krull <flo.krull@gmail.com>
2020-07-27 14:10:00 +0200

    maxmem: same results if number of cores is changed

--- src/1_clean_bio.sh
+++ src/1_clean_bio.sh
@@ -9,14 +9,13 @@ clean_bio() {
   echo "cleaning bio with ${OMP_NUM_THREADS} CPUs and ${CFG_MAXMEM_KB} kB memory"
   python ${PPROOT}/pdb-merge-bio/merge_bio_folder.py \
     --numthreads ${OMP_NUM_THREADS} \
-    --maxmem ${CFG_MAXMEM_KB} \
+    --maxmem $((CFG_MAXMEM_KB / OMP_NUM_THREADS )) \
     --src ${pp_in_pdbbio} \
     --dst ${pp_tmp_prefix}/pdb_bio_merged \
     2>&1 | pplog 1
 
   mv ${pp_tmp_prefix}/pdb_bio_merged ${dst_dir}
   [ -d ${dst_dir} ] || { printf "error: pdb directory not created\n"; exit 1; }
-   #${pp}/bin/0pdbbio_merge_model.sh pdb pdb_bio_merged/ pdb_dst | pplog 1
 }
 
 clean_bio
