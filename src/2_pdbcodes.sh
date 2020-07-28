f09b2b4745c3d9c8b42df4248edf96e2465c7118
Florian Krull <flo.krull@gmail.com>
2020-07-28 22:46:55 +0200

    pdbcodes: fix log msg position

--- src/2_pdbcodes.sh
+++ src/2_pdbcodes.sh
@@ -1,9 +1,9 @@
 
 if [ ! -f ${pp_out_prefix}_pdbcodes ]; then
+  printf "generating PDB codes...\n" | pplog 0
   mkdir -p ${pp_tmp_prefix}
   find ${pp_in_pdb} -name '*.pdb' -exec basename {} .pdb \; | sort > ${pp_tmp_prefix}/pdbcodes
   mv ${pp_tmp_prefix}/pdbcodes ${pp_out_prefix}_pdbcodes
-  printf "generating PDB codes...\n" | pplog 0
   printf "using %s PDB codes\n" $( cat ${pp_out_prefix}_pdbcodes | wc -l ) | pplog 0
 else
   printf "using existing PDB codes (n=%s)\n" $( cat ${pp_out_prefix}_pdbcodes | wc -l ) | pplog 0
